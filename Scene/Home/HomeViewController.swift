736ad7a7714e2184e4e57e2b75b51bf6499bc69a
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-16 16:59:27 +0300

    HomeViewController created

---
+++ Scene/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 16.04.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
