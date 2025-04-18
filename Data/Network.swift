f6be46e226b73dec29941b294698f19233160fce
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-18 12:26:17 +0300

    Update Network.swift
    Network Class was fragmented using extension

--- Data/Network.swift
+++ Data/Network.swift
@@ -20,3 +20,36 @@ class Network {
     }
     
 }
+
+//Get Methots
+extension Network {
+    
+    func getOne<T: Decodable>(of type: T, with query: Query, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        query.getDocuments { snapshot, error in
+            
+            if let error {
+                print("Error: \(#function) couldn't access snapshot, \(error)")
+                completion(.failure(error))
+                return
+            }
+            
+            guard let document = snapshot?.documents.first else {
+                print("Warning: \(#function) document not found")
+                completion(.failure(NetworkError.documentNotFound))
+                return
+            }
+            
+            do {
+                let data = try document.data(as: T.self)
+                completion(.success(data))
+            } catch let error {
+                print("Error: \(#function) document not decoded from data, \(error)")
+                completion(.failure(error))
+                return
+            }
+            
+            
+        }
+    }
+}
