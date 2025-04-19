5bb06759d8ae477be20745ca31354ea71573231c
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-19 14:52:03 +0300

    Update Network.swift
    Single and multiple document fetch operations are defined with the query parameter.

--- Data/Network.swift
+++ Data/Network.swift
@@ -24,6 +24,7 @@ class Network {
 //Get Methots
 extension Network {
     
+    // For Single Documents
     func getOne<T: Decodable>(of type: T, with query: Query, completion: @escaping (Result<T, Error>) -> Void) {
         
         query.getDocuments { snapshot, error in
@@ -52,4 +53,43 @@ extension Network {
             
         }
     }
+    
+    // For Multiple Documents
+    func getMany<T: Decodable>(of type: T.Type,
+                               with query: Query,
+                               completion: @escaping (Result<([T]), Error>) -> Void) {
+        
+        var decodeErrors: [Error] = []
+        
+        query.getDocuments { snapshot, error in
+            
+            if let error {
+                print("Error: \(#function) couldn't access snapshot, \(error)")
+                return
+            }
+            
+            var responses: [T] = []
+            
+            guard let documents = snapshot?.documents else {
+                print("Warning: \(#function) documents not found")
+                completion(.success(responses))
+                return
+            }
+            
+            for document in documents {
+                do {
+                    let data = try document.data(as: T.self)
+                    responses.append(data)
+                } catch let error {
+                    decodeErrors.append(error)
+                }
+            }
+            
+            print("Error: \(#function) documents not decoded from data, \(decodeErrors)")
+            completion(.success(responses))
+            
+        }
+    }
 }
+
+
