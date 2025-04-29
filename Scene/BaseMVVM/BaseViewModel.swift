4627d07106c664891c574210489ad1ad6f72a1ae
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-29 18:28:16 +0300

    Update BaseViewModel.swift
    Animations and alert message closures adjusted

--- Scene/BaseMVVM/BaseViewModel.swift
+++ Scene/BaseMVVM/BaseViewModel.swift
@@ -5,6 +5,13 @@
 //  Created by MacBook on 16.04.2025.
 //
 
+import UIKit
+
 class BaseViewModel {
-    
+    var loadingAnimationStart: ((String) -> Void)?
+    var loadingAnimationStop: (() -> Void)?
+    var successAnimation: ((String) -> Void)?
+    var failAnimation: ((String) -> Void)?
+    var alertMessage: ((String, String ,String, ((UIAlertAction) -> Void)?) -> Void)?
+    var gradientLoagingTabAnimation: CustomGradientLoadingAnimation?
 }
