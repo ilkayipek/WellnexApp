a8c936b4e2efea1b8482bebc81abafe1b2d4353c
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-29 14:24:07 +0300

    SignUp MVVM created

---
+++ Scene/SignUp/SignUpViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 29.04.2025.
+//
+
+import UIKit
+
+class SignUpViewController: BaseViewController<SignUpViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel = SignUpViewModel()
+       
+    }
+
+}
---
+++ Scene/SignUp/SignUpViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SignUpViewModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 29.04.2025.
+//
+
+class SignUpViewModel: BaseViewModel {
+    
+}
