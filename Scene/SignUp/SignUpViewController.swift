248a41906859b47401e159a99975c3f2c307eee1
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-04-30 17:55:50 +0300

    Update SignUpViewController.swift
    sign up scene properties defined.
    Sign Up button and sign in button action funcs defined.

--- Scene/SignUp/SignUpViewController.swift
+++ Scene/SignUp/SignUpViewController.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class SignUpViewController: BaseViewController<SignUpViewModel> {
+    
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +22,25 @@ class SignUpViewController: BaseViewController<SignUpViewModel> {
        
     }
 
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        let fullName = fullNameTextField.text ?? ""
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let passwordConf = passwordConfirmationTextField.text ?? ""
+        
+        
+        viewModel?.signUp(fullName, email, password, passwordConf: passwordConf) { [weak self] status in
+            guard let self else { return }
+            
+            self.successCreateAccount()
+        }
+        
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+    }
+    
+    func successCreateAccount() {
+        
+    }
 }
