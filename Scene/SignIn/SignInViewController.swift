28d32c86112b01af5d6f6e4e41595ce9d09b8b58
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-01 20:27:29 +0300

    SignIn MVVM created

---
+++ Scene/SignIn/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 1.05.2025.
+//
+
+import UIKit
+
+class SignInViewController: BaseViewController<SignInViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel = SignInViewModel()
+        
+    }
+
+}
---
+++ Scene/SignIn/SignInViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  SignIn.swift
+//  WellnexApp
+//
+//  Created by MacBook on 1.05.2025.
+//
+
+class SignInViewModel: BaseViewModel {
+    
+}
