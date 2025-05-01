195987e836ca38b80009c081067818856f3fc002
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-01 19:57:42 +0300

    Update SignUpViewModel.swift
    userType check alert message function defined.
    
    userType check function run before network process start

--- Scene/SignUp/SignUpViewModel.swift
+++ Scene/SignUp/SignUpViewModel.swift
@@ -9,31 +9,40 @@ import Foundation
 
 class SignUpViewModel: BaseViewModel {
     
-    func signUp(_ fullName: String, _ email: String, _ password: String,passwordConf: String, _ closure: @escaping (Bool) -> Void) {
-        
-        if isPasswordEqual(password, passwordConf) {
-            if isValidEmail(email: email) {
-                if validatePassword(password) {
-                    loadingAnimationStart?("Lütfen Bekleyin..")
-                    AuthManager.shared.signUpWithEmail(fullName, email, password) { [weak self] status, error in
-                        guard let self else {return}
-                        if let error {
-                            self.failAnimation?("Bir Hata Oluştu!:\(error.localizedDescription)")
-                            closure(false)
-                        } else if status {
-                            self.successAnimation?("Kayıt işlemi başarılı")
-                            closure(true)
+    func signUp(_ fullName: String, _ email: String, _ password: String,_ passwordConf: String, _ userType: UserType ,_ closure: @escaping (Bool) -> Void) {
+            
+            if isPasswordEqual(password, passwordConf) {
+                if isValidEmail(email: email) {
+                    if validatePassword(password) {
+                        
+                        userTypeAlertMessage(userType: userType) { [weak self]  in
+                            guard let self else {return}
+                            
+                            loadingAnimationStart?("Lütfen Bekleyin..")
+                            
+                            AuthManager.shared.signUpWithEmail(fullName, email, password) { [weak self] status, error in
+                                guard let self else {return}
+                                if let error {
+                                    self.failAnimation?("Bir Hata Oluştu!:\(error.localizedDescription)")
+                                    closure(false)
+                                } else if status {
+                                    self.successAnimation?("Kayıt işlemi başarılı")
+                                    closure(true)
+                                }
+                            }
                         }
+                    } else {
+                        failAnimation?("Şifreniz en az 8 karakter uzunluğunda olmalıdır ve en az bir büyük harf, bir küçük harf, bir sayı ve bir özel karakter içermelidir. Örneğin: Abcd1234!")
                     }
                 } else {
-                    failAnimation?("Şifreniz en az 8 karakter uzunluğunda olmalıdır ve en az bir büyük harf, bir küçük harf, bir sayı ve bir özel karakter içermelidir. Örneğin: Abcd1234!")
+                    failAnimation?("Lütfen geçerli bir Eposta giriniz.")
                 }
             } else {
-                failAnimation?("Lütfen geçerli bir Eposta giriniz.")
+                failAnimation?("Şifreler uyuşmuyor. Lütfen tekrar deneyiniz.")
             }
-        } else {
-            failAnimation?("Şifreler uyuşmuyor. Lütfen tekrar deneyiniz.")
-        }
+        
+        
+        
         
         
         
@@ -54,6 +63,15 @@ class SignUpViewModel: BaseViewModel {
         let passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"
         let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
         return passwordPredicate.evaluate(with: password)
+    }
+    
+    private func userTypeAlertMessage(userType: UserType, _ closure: @escaping(() -> Void)) {
+        
+        alertMessage?("", "\(userType.rawValue) olarak devam etmek istediğinize emin misiniz? Bu seçim sonradan değiştirilemez.","Devam Et") { _ in
+            
+            closure()
+        }
+        
     }
     
 }
