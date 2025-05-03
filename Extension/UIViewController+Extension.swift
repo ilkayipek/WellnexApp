36ba92bb93874c2a31b57123181a6f3c24aa6afc
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-03 14:51:47 +0300

    Create UIViewController+Extension.swift
    This extension allows to load viewController from xib file.
    Warning: Xib file and viewController must have the same name.

---
+++ Extension/UIViewController+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 16.04.2025.
+//
+
+import UIKit
+
+extension UIViewController {
+    static func loadFromNib() -> Self {
+        func instantiateFromNib<T: UIViewController>() -> T {
+            return T.init(nibName: String(describing: T.self), bundle: nil)
+        }
+        
+        return instantiateFromNib()
+    }
+}
