771c1758fb6bf4a31eaec94abc404566c39e4346
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-04 16:53:31 +0300

    My Profile MVVM Created

---
+++ Scene/MyProfile/MyProfileVideModel.swift
@@ -0,0 +1,10 @@
+//
+//  MyProfileVideModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 4.05.2025.
+//
+
+class MyProfileVideModel: BaseViewModel {
+    
+}
---
+++ Scene/MyProfile/MyProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MyProfileViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 4.05.2025.
+//
+
+import UIKit
+
+class MyProfileViewController: BaseViewController<MyProfileVideModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel = MyProfileVideModel()
+    }
+
+}
