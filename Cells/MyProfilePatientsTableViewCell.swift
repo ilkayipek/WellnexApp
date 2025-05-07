8a57667eed544e6c1cb54374ce3bad1015154bb5
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-07 18:56:37 +0300

    Create MyProfilePatientsTableViewCell.swift

---
+++ Cells/MyProfilePatientsTableViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  MyProfilePatientsTableViewCell.swift
+//  WellnexApp
+//
+//  Created by MacBook on 5.05.2025.
+//
+
+import UIKit
+
+class MyProfilePatientsTableViewCell: UITableViewCell {
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var relationStatus: UILabel!
+    @IBOutlet weak var profileImage: CustomUIImageView!
+    @IBOutlet weak var relationshipNote: UILabel!
+    @IBOutlet weak var containerView: CustomContainerUIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func loadCell(relationshipModel: PatientDoctorRelationshipModel) {
+        fullName.text = relationshipModel.patientName
+        relationStatus.text = relationshipModel.status
+        relationshipNote.text = relationshipModel.notes
+        
+        let statusEnum = RelationshipStatus(rawValue: relationshipModel.status) ?? .pending
+        
+        switch statusEnum {
+        case .pending:
+            
+            containerView.borderColor = UIColor.lightGray
+        case .accepted:
+            
+            containerView.borderColor = UIColor.systemGreen
+        case .rejected:
+            
+            containerView.borderColor = UIColor.systemRed
+        }
+    }
+    
+}
