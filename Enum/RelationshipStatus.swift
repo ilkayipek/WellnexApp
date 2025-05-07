62da945cceff8db7f1bef4c0cc6175081ebcccef
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-07 18:57:46 +0300

    Create RelationshipStatus.swift

---
+++ Enum/RelationshipStatus.swift
@@ -0,0 +1,13 @@
+//
+//  patientDoctorRelationStatus.swift
+//  WellnexApp
+//
+//  Created by MacBook on 5.05.2025.
+//
+
+enum RelationshipStatus: String {
+    
+    case pending = "Pending"
+    case accepted = "Accepted"
+    case rejected = "Rejected"
+}
