c5b19b41691d5cab5363408b04069cddb791ab55
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-10 20:36:10 +0300

    MVVM created

---
+++ Scene/Doctor/AssignTask/AssignTaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AssignTaskViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 10.05.2025.
+//
+
+import UIKit
+
+class AssignTaskViewController: BaseViewController<AssignTaskViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel = AssignTaskViewModel()
+       
+    }
+
+}
---
+++ Scene/Doctor/AssignTask/AssignTaskViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  AssignTaskViewModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 10.05.2025.
+//
+
+class AssignTaskViewModel: BaseViewModel {
+    
+}
