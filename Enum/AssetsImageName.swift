324df5fbdde4ce3cc920f7bfcb6c21a2affe9f4b
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-18 15:14:45 +0300

    Create AssetsImageName.swift

---
+++ Enum/AssetsImageName.swift
@@ -0,0 +1,16 @@
+//
+//  AssetsImageName.swift
+//  WellnexApp
+//
+//  Created by MacBook on 16.05.2025.
+//
+
+enum AssetsImageName: String {
+    case bloodPressure
+    case bloodSugar
+    case pulse
+    case bodyTemperature
+    case patient
+    case doctor
+    case logo
+}
