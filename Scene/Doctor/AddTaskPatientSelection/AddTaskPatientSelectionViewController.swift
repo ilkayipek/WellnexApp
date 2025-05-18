4124b194765606cac3727260f97551cba8eaf32d
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-18 15:28:06 +0300

    MVVM created

---
+++ Scene/Doctor/AddTaskPatientSelection/AddTaskPatientSelectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddTaskPatientSelectionViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 18.05.2025.
+//
+
+import UIKit
+
+class AddTaskPatientSelectionViewController: BaseViewController<AddTaskPatientSelectionViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel = AddTaskPatientSelectionViewModel()
+    }
+
+
+}
---
+++ Scene/Doctor/AddTaskPatientSelection/AddTaskPatientSelectionViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  AddTaskPatientSelectionViewModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 18.05.2025.
+//
+
+class AddTaskPatientSelectionViewModel: BaseViewModel {
+    
+}
