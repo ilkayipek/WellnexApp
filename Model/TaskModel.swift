25aff9617ef8a6c5cac6f70115770f76ac5172a4
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-18 15:15:10 +0300

    Create TaskModel.swift

---
+++ Model/TaskModel.swift
@@ -0,0 +1,24 @@
+//
+//  TaskModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 16.05.2025.
+//
+
+import Foundation
+
+struct TaskModel: FirebaseIdentifiable {
+    var id: String
+    let assignedBy: String
+    let assignedTo: String
+    let description: String
+    let measureTypeId: String
+    let patientId: String
+    let relationId: String
+    let startDate: Date
+    let endDate: Date
+    let timeSlots: [DayOfWeek: [TimeSlotPeriod]]
+}
+
+
+
