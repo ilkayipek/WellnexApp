a705a17a9dd02008d48ff5d25939cb50ebcfbeb7
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-21 13:21:18 +0300

    Update TimeSlotPeriod.swift

--- Enum/TimeSlotPeriod.swift
+++ Enum/TimeSlotPeriod.swift
@@ -9,7 +9,7 @@ enum TimeSlotPeriod: String, CaseIterable, Codable {
     case morning = "07:00-10:00"
     case afternoon = "12:00-15:00"
     case evening = "18:00-22:00"
-    case night = "22:00-00:00"
+    case night = "22:00-23:59"
 
     var startTime: String {
         switch self {
@@ -25,7 +25,7 @@ enum TimeSlotPeriod: String, CaseIterable, Codable {
         case .morning: return "10:00"
         case .afternoon: return "15:00"
         case .evening: return "22:00"
-        case .night: return "00:00"
+        case .night: return "23:59"
         }
     }
 }
