a856f2e22f741182b2a0611d8c0834ae33971c4b
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-21 16:07:44 +0300

    MVVM created

---
+++ Scene/Patient/CompleteTaskInstance/CompleteTaskInstanceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CompleteTaskInstanceViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 21.05.2025.
+//
+
+import UIKit
+
+class CompleteTaskInstanceViewController: BaseViewController<CompleteTaskInstanceViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel = CompleteTaskInstanceViewModel()
+    }
+
+
+
+}
---
+++ Scene/Patient/CompleteTaskInstance/CompleteTaskInstanceViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  CompleteTaskInstanceViewModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 21.05.2025.
+//
+
+class CompleteTaskInstanceViewModel: BaseViewModel {
+    
+}
