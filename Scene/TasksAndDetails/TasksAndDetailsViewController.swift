3638990c9801e0a81213d5b30acea50f5426afca
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-22 16:54:25 +0300

    Create TasksAndDetailsViewController.swift

---
+++ Scene/TasksAndDetails/TasksAndDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TasksAndDetailsViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 22.05.2025.
+//
+
+import UIKit
+
+class TasksAndDetailsViewController: BaseViewController<TaskAndDetailsViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       viewModel = TaskAndDetailsViewModel()
+    }
+
+    
+}
