feb3c447d35d50ebd9735aae63eec7336cbd4b97
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-24 14:31:58 +0300

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -11,9 +11,9 @@ import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
@@ -28,17 +28,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
         
         Messaging.messaging().delegate = self
+        
+        let activeColor = UIColor.activePrimaryButton
+        
+        UITabBar.appearance().tintColor = activeColor
+        UITabBar.appearance().unselectedItemTintColor = UIColor.lightGray
+        
+        UINavigationBar.appearance().tintColor = activeColor
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: activeColor]
+        
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -46,11 +56,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     }
     
     func application(_ application: UIApplication,
-                      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
-       //APNs token'ını FCM ile ilişkilendir
-       Messaging.messaging().apnsToken = deviceToken
-     }
-
+        //APNs token'ını FCM ile ilişkilendir
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
 }
 
