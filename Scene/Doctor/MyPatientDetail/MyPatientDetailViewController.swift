4ba45352d5d5517d9128697cd11f6946e8cf3a6c
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-26 12:12:14 +0300

    MVVM created

---
+++ Scene/Doctor/MyPatientDetail/MyPatientDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MyPatientDetailViewController.swift
+//  WellnexApp
+//
+//  Created by MacBook on 26.05.2025.
+//
+
+import UIKit
+
+class MyPatientDetailViewController: BaseViewController<MyPatientDetailViewModel> {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel = MyPatientDetailViewModel()
+    }
+
+
+    
+
+}
---
+++ Scene/Doctor/MyPatientDetail/MyPatientDetailViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  MyPatientDetailViewModel.swift
+//  WellnexApp
+//
+//  Created by MacBook on 26.05.2025.
+//
+
+class MyPatientDetailViewModel: BaseViewModel {
+    
+}
