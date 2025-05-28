d3be8fb9c359ae5b073446553cddfc9f481ed381
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-05-28 20:18:35 +0300

    Create ReportsTableViewCell.swift

---
+++ Cells/ReportsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReportsTableViewCell.swift
+//  WellnexApp
+//
+//  Created by MacBook on 28.05.2025.
+//
+
+import UIKit
+
+
+class ReportsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
