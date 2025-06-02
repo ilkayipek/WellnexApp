561fa5967bce3d9e5edf69aec66b9ad5b13009ad
ilkay-ipek <ilkayy.ipek@gmail.com>
2025-06-02 14:35:52 +0300

    Update ReportsViewController.swift

--- Scene/Reports/ReportsViewController.swift
+++ Scene/Reports/ReportsViewController.swift
@@ -8,31 +8,76 @@
 import UIKit
 
 class ReportsViewController: BaseViewController<ReportsViewModel> {
+    
     @IBOutlet weak var reportsTableView: UITableView!
+    @IBOutlet weak var emptyMessageView: CustomContainerUIView!
+    
+    private var reports = [ReportModel]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         viewModel = ReportsViewModel()
+        configureTableView()
+        getReports()
     }
     
-    func postReport(taskId: String, patientId: String, doctorId: String) {
+    private func configureTableView() {
+        reportsTableView.delegate = self
+        reportsTableView.dataSource = self
         
-        let reportModel = ReportModel(id: UUID().uuidString, taskId: taskId, patientId: patientId, doctorId: doctorId, isComplete: false)
+        reportsTableView.registerCellFromNib(ReportsTableViewCell.self)
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(getReports), for: .valueChanged)
+        reportsTableView.refreshControl = refreshControl
+    }
+    
+    func postReport(task: TaskModel, patientId: String, doctorId: String) {
+        
+        let reportModel = ReportModel(id: UUID().uuidString, taskId: task.id, patientId: patientId, doctorId: doctorId, isComplete: false, taskModel: task)
+        
+        reports.insert(reportModel, at: 0)
+        reportsTableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
         
         viewModel?.postReportModel(reportModel) { [weak self] status in
             guard let self else { return }
             guard status else {return}
             
-            
-            
+            self.getReports()
         }
         
     }
     
-    private func loadReports() {
+    @objc private func getReports() {
+        
+        viewModel?.fetchReports { [weak self] reports in
+            guard let self else {return}
+            
+            self.reports = reports
+            self.emptyMessageView.isHidden = !reports.isEmpty
+            self.reportsTableView.reloadData()
+            self.reportsTableView.refreshControl?.endRefreshing()
+        }
         
     }
 
+}
+
+extension ReportsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return reports.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = reportsTableView.sameNameDequeueReusableCell(ReportsTableViewCell.self, indexPath: indexPath)
+        
+        let report = reports[indexPath.row]
+        cell.loadCell(report)
+        
+        return cell
+    }
+    
+    
 }
